5e2653ba8d02a3b43397775a763756cda1d8a69d
Cassandraaaaa <107783946+Cassandraaaaa@users.noreply.github.com>
2022-07-22 19:51:04 +0800

    Add files via upload

---
+++ updated_stock_market/app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template, request, flash
+
+app = Flask(__name__)
+app.secret_key = "itm_is_fun<333"
+
+@app.route("/stock", methods=['POST', 'GET'] )
+def index():
+    flash("What is the stock market value in this country?")
+    return render_template("index.html")
+
+@app.route("/show", methods=['POST', 'GET'])
+def show():
+    flash("This is the stock market value in " + str(request.form["name_input"]))
+    return render_template("index.html")
