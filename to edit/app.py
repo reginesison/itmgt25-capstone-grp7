1e9de68c46f199a336826bced27e65221b23df92
Regine Sison <regine.sison@obf.ateneo.edu>
2022-07-26 21:04:09 +0800

    changes

--- to edit/app.py
+++ to edit/app.py
@@ -13,20 +13,26 @@ soup = BeautifulSoup(html, features="lxml")
 tds = soup.find_all('td')
 size = 4
 
-tempList = []
-for td in tds:
-    tempList.append(td.text.strip())
-
-finalList = [tempList[n:n+size] for n in range(0, len(tempList), size)]
-for item in finalList:
-    answer = str((item[0] + "'s stock market value is at $" + item[1] + " in 2020 and is in rank #" + item[2] + "."))
 
 @app.route("/stock", methods=['POST', 'GET'])
 def index():
     flash("What is the stock market value in this country?")
-    return render_template("index.html",answer=answer)
+    return render_template("index.html",answer="What is the stock market value in this country?")
 
 @app.route("/show", methods=['POST', 'GET'])
 def show():
-    flash(answer)
-    return render_template("index.html",answer=answer)
+    country = request.form['name_input']
+    # flash(country)
+    tempList = []
+    # answer_item=""
+    for td in tds:
+        tempList.append(td.text.strip())
+
+    finalList = [tempList[n:n+size] for n in range(0, len(tempList), size)]
+
+    for item in finalList:
+        if item[0] == country: 
+            answer_item = str((item[0] + "'s stock market value is at $" + item[1] + " in 2020 and is in rank #" + item[2] + "."))
+        else: 
+            answer_item = "No data on country."
+    return render_template("index.html",answer=answer_item)
